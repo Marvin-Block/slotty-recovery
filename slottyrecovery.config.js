58a841a2494a760023cdf665abc3f8d376029ce5
Marvin Block <mabl@dcc.ruhr>
2025-04-11 11:49:21 +0200

    updated pm2 config

--- slottyrecovery.config.js
+++ slottyrecovery.config.js
@@ -3,7 +3,7 @@ module.exports = {
         name: "slottyrecovery",
         max_restarts: 100,
         script: "node_modules/next/dist/bin/next",
-        args: "start",
+        args: "start -p 3001",
         // instances: "max",
         // exec_mode: "cluster",
         autorestart: true,
